adb9bd4ed4860781a3df8a62bc17d0db98571fc1
prabhath <prabhathkiran20@gmail.com>
2015-12-25 00:10:02 -0600

    Add postgresql as db and create a table for signups(model) and make this model appear in admin page.

--- django_signup/settings.py
+++ django_signup/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'signups',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'django_signup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_signups_db',
+        'USER': 'kiran',
+        'PASSWORD': 'rtyu',
+        'HOST': '',
+        'POSRT': '',
     }
 }
 
--- signups/models.py
+++ signups/models.py
@@ -1,5 +1,17 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import smart_unicode
+
 
 # Create your models here.
+class SignUp(models.Model):
+
+    first_name = models.CharField(max_length=120, null=True, blank=True)
+    last_name = models.CharField(max_length=120, null=True, blank=True)
+    email = models.EmailField()
+    timestamp_add = models.DateTimeField(auto_now_add=True, auto_now=False, null=True)
+    timestamp_update = models.DateTimeField(auto_now_add=False, auto_now=True, null=True)
+
+    def __unicode__(self):
+        return smart_unicode(self.first_name)
